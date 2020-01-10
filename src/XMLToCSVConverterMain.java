de60488a40bbe1517743e14c045dfeaf1ac7cec1
Asif Foysal Meem <asif.foysalm@gmail.com>
2020-01-10 11:51:09 +1100

    Created CSVGenerator Class to generate CSV Created XMLReader Class to read xml

--- src/XMLToCSVConverterMain.java
+++ src/XMLToCSVConverterMain.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,15 +9,26 @@
 
 /**
  *
- * @author User
+ * @author Asif Meem
  */
 public class XMLToCSVConverterMain {
 
     /**
      * @param args the command line arguments
      */
+    private final static String inputFilePath = "C:\\Users\\User\\Documents\\NetBeansProjects\\Input\\testfile.xml";
+    private final static String outputFilePath = "C:\\Users\\User\\Documents\\NetBeansProjects\\Processed\\";
+    private final static String extractionElement = "CSVIntervalData";
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        XMLReader xml = new XMLReader(inputFilePath, extractionElement);
+        ArrayList<String> xmlArr = new ArrayList(xml.getXmlArrayList());
+        
+        for (String element : xmlArr){
+            System.out.println(element);
+        }
     }
     
 }
